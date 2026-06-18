e4a97ca38f40e302c2522d5d0372ed5643f42217
MEDDy-account <maehimekichi0902@gmail.com>
2026-06-18 12:53:27 +0900

    Update payment.py

--- payment_system/payment.py
+++ payment_system/payment.py
@@ -7,43 +7,45 @@ pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 )
 
-img = Image.open("pay_driver.png")
+import os
 
-# 正しい向き
-img = img.rotate(270, expand=True)
+png_files = [f for f in os.listdir(".") if f.endswith(".png")]
 
-text = pytesseract.image_to_string(
-    img,
-    config="--psm 6"
-)
+print(png_files)
 
-print("=== OCR結果 ===")
-print(text)
+for file in png_files:
+    print(file)
 
-# 数字を全部取得
-# 数字を全部取得
-nums = [int(x) for x in re.findall(r"\d+", text)]
+    img = Image.open(file)
 
-print("\n抽出数字")
-print(nums)
+    # 正しい向き
+    img = img.rotate(270, expand=True)
 
-if len(nums) < 8:
-    raise ValueError("必要な数字が取得できませんでした")
+    text = pytesseract.image_to_string(
+        img,
+        config="--psm 6"
+    )
 
-# OCR結果の位置から取得
-b1 = nums[3]
-b2 = nums[7]
+    print("=== OCR結果 ===")
+    print(text)
 
-print(f"宅急便個数: {b1}")
-print(f"ネコポス個数: {b2}")
+    nums = [int(x) for x in re.findall(r"\d+", text)]
 
-fee1 = b1 * 180
-fee2 = b2 * 160
+    print("\n抽出数字")
+    print(nums)
 
-total = fee1 + fee2
+    if len(nums) < 8:
+        print(f"{file} はスキップ")
+        continue
 
-print("\n===== 集計 =====")
-print(f"宅急便売上: {fee1:,}円")
-print(f"ネコポス売上: {fee2:,}円")
-print(f"合計: {total:,}円")
-print("sales_history.csv に保存しました")
+    b1 = nums[3]
+    b2 = nums[7]
+
+    fee1 = b1 * 180
+    fee2 = b2 * 160
+    total = fee1 + fee2
+
+    print(f"\n{file}")
+    print(f"宅急便個数: {b1}")
+    print(f"ネコポス個数: {b2}")
+    print(f"合計: {total:,}円")
